f9f68d66df467b8207b98183571def2b59155b4c
fmadihi <fmadihi.2011@gmail.com>
2025-05-17 15:02:51 +0330

    useState

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import {Classes} from "./components/Classes";
+import { Classes } from "./components/Classes";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   const Job = (props) => {
     return (
       <div>
-        {props.salary} ,
-        {props.position} ,
-        {props.company}
-      </div> 
+        {props.salary} ,{props.position} ,{props.company}
+      </div>
     );
   };
 
   const classes = [
-    {name: "html", finished: true},
-    {name: "Javascript", finished: false},
-    {name: "css", finished: true},
-    {name: "bootstrap", finished: true},
-    {name: "react", finished: false},
+    { name: "html", finished: true },
+    { name: "Javascript", finished: false },
+    { name: "css", finished: true },
+    { name: "bootstrap", finished: true },
+    { name: "react", finished: false },
   ];
-
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <Job salary={1111} position="MOdir" company="IT" />
-      {classes.map((classe,index)=>{
-        return <Classes key={index} name={classe.name} finished={classe.finished} />
-      })} 
+      {classes.map((classe, index) => {
+        return (
+          <Classes key={index} name={classe.name} finished={classe.finished} />
+        );
+      })}
+
+      <div>
+        <button onClick={() => setCount(count + 1)}>increase</button>
+        <button onClick={() => setCount(count - 1)}>decrease</button>
+        <button onClick={() => setCount(0)}>set to 0</button>
+        <h3>{count}</h3>
+      </div>
     </div>
   );
 }
