2bf7efab29c6f67bccbf0b73dc0b8a7584acbf96
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2025-05-17 12:33:02 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import {Classes} from "./components/Classes";
+import "./App.css";
+
+function App() {
+  const Job = (props) => {
+    return (
+      <div>
+        {props.salary} ,
+        {props.position} ,
+        {props.company}
+      </div> 
+    );
+  };
+
+  const classes = [
+    {name: "html", finished: true},
+    {name: "Javascript", finished: false},
+    {name: "css", finished: true},
+    {name: "bootstrap", finished: true},
+    {name: "react", finished: false},
+  ];
+
+  return (
+    <div className="App">
+      <Job salary={1111} position="MOdir" company="IT" />
+      {classes.map((classe,index)=>{
+        return <Classes key={index} name={classe.name} finished={classe.finished} />
+      })} 
+    </div>
+  );
+}
+
+export default App;
